23cd75b3d78f8e9ac0d3c24288674b8d0d801d7b
Jarod Reid <therealfugu@gmail.com>
2020-09-25 14:28:32 +0000

    update install script

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 sudo apt-get update
-sudo apt-get install docker-compose zsh
+sudo apt-get install docker-compose zsh yadm
+yadm clone https://github.com/fugufish/dotfiles.git
+yadm reset --hard
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install neovim
+pip3 install neovim
