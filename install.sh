7c726b3450659490b233ab73cfd32218f2e69232
Jarod Reid <jarod@solidalchemy.com>
2020-09-30 15:21:22 +0000

    add monogo shell to tools

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 sudo apt-get update
-sudo apt-get install docker-compose zsh yadm
+sudo apt-get install -y zsh yadm
+sudo apt-get install -y gnupg
 yadm clone https://github.com/fugufish/dotfiles.git
 yadm reset --hard
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew install neovim
 pip3 install neovim
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+wget https://repo.mongodb.org/apt/debian/dists/stretch/mongodb-org/4.4/main/binary-amd64/mongodb-org-shell_4.4.1_amd64.deb
+sudo dpkg -i mongodb-org-shell_4.4.1_amd64.deb      
