b8af5bf66c57b4991e33a2df8d47170e7b3d8d5b
Jarod Reid <therealfugu@gmail.com>
2020-09-30 20:08:44 +0000

    update script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y zsh yadm
 sudo apt-get install -y gnupg
 yadm clone https://github.com/fugufish/dotfiles.git
 yadm reset --hard
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+CI=1/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew install neovim
 pip3 install neovim
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
