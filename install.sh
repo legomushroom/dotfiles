daee2671f9ba17cbbfaf0b1b6a2638b240692cbb
Jarod Reid <therealfugu@gmail.com>
2020-09-30 21:25:56 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ sudo apt-get install -y zsh yadm
 sudo apt-get install -y gnupg
 yadm clone https://github.com/fugufish/dotfiles.git
 yadm reset --hard
-CI=1/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew install neovim
-pip3 install neovim
+#CI=1/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+#brew install neovim
+#pip3 install neovim
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 wget https://repo.mongodb.org/apt/debian/dists/stretch/mongodb-org/4.4/main/binary-amd64/mongodb-org-shell_4.4.1_amd64.deb
