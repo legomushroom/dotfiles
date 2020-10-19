24994a1eca79aa727c7a2bd0b1e4dd6ba90285d9
Jarod Reid <jarod@solidalchemy.com>
2020-10-19 12:57:39 +0000

    install byobu

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y zsh yadm
-sudo apt-get install -y gnupg
+sudo apt-get install -y zsh yadm byobu gnupg
 yadm clone https://github.com/fugufish/dotfiles.git
 yadm reset --hard
 #CI=1/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -8,5 +7,3 @@ yadm reset --hard
 #pip3 install neovim
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-wget https://repo.mongodb.org/apt/debian/dists/stretch/mongodb-org/4.4/main/binary-amd64/mongodb-org-shell_4.4.1_amd64.deb
-sudo dpkg -i mongodb-org-shell_4.4.1_amd64.deb      
