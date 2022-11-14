23e06b7476c66217b4c6db33ee794bc0ac03c420
Oleg Solomka <legomushroom@gmail.com>
2022-11-14 15:16:46 -0800

    Update .bashrc-functions.sh

--- .bashrc-functions.sh
+++ .bashrc-functions.sh
@@ -161,3 +161,13 @@ function wmsg() {
 
     watch -n 1 "dmesg | tail --lines $len"
 }
+
+# Install GitHub CLI.
+function igh() {
+  type -p curl >/dev/null || sudo apt install curl -y
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
+}
